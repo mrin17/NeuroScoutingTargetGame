1b5bac26bbaa9f156e84ba21e2d8db8f649dd054
Michael Rinaldi <mikerinaldi1@yahoo.com>
2015-10-22 15:32:06 -0400

    Created scrBaseball, set appear time
    The baseball only appears for 2 seconds and then is destroyed

---
+++ NeuroScoutingTargetGame/Assets/Scripts/scrBaseball.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Mike - script to be put on the baseball. Controls movement and scale
+public class scrBaseball : MonoBehaviour {
+
+    public float appearTimeMax = 2f;
+    float appearTimer = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        appearTimer += Time.deltaTime;
+        if (appearTimer > appearTimeMax)
+            Destroy(gameObject);
+	}
+
+}
