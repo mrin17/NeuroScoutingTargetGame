f15f919cb4edfeafef2bc4707a35717912fef914
Michael Rinaldi <mikerinaldi1@yahoo.com>
2015-10-22 15:36:37 -0400

    Baseball now (accurately) grows bigger
    As time goes on, the baseball grows bigger as it gets closer to you

--- NeuroScoutingTargetGame/Assets/Scripts/scrBaseball.cs
+++ NeuroScoutingTargetGame/Assets/Scripts/scrBaseball.cs
@@ -4,6 +4,8 @@ using System.Collections;
 //Mike - script to be put on the baseball. Controls movement and scale
 public class scrBaseball : MonoBehaviour {
 
+    public float minScale = .1f;
+    public float maxScale = 1f;
     public float appearTimeMax = 2f;
     float appearTimer = 0f;
 
@@ -14,6 +16,11 @@ public class scrBaseball : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //as time goes by, increase the size of the baseball
+        float scaleRatio = appearTimer / appearTimeMax * (maxScale - minScale) + minScale;
+        transform.localScale = new Vector3(scaleRatio, scaleRatio, 1);
+
+        //only have the baseball appear for appearTiemMax seconds, then destroy the baseball
         appearTimer += Time.deltaTime;
         if (appearTimer > appearTimeMax)
             Destroy(gameObject);
