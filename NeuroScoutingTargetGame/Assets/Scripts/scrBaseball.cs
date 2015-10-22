2caed2625ba41c7fe4648114b7564aa0535cecc4
Michael Rinaldi <mikerinaldi1@yahoo.com>
2015-10-22 15:45:47 -0400

    random velocity on baseball
    -Added a rigidbody2D to preBaseball
    -randomized force is added in Start() of scrBaseball

--- NeuroScoutingTargetGame/Assets/Scripts/scrBaseball.cs
+++ NeuroScoutingTargetGame/Assets/Scripts/scrBaseball.cs
@@ -4,14 +4,26 @@ using System.Collections;
 //Mike - script to be put on the baseball. Controls movement and scale
 public class scrBaseball : MonoBehaviour {
 
-    public float minScale = .1f;
+    public float minScale = 0f;
     public float maxScale = 1f;
+
     public float appearTimeMax = 2f;
+
+    public Vector3 startPos = new Vector3(0, 0, 0);
+
+    public float maxHSpeed = 20f;
+    public float maxVSpeed = 10f;
+
+    Rigidbody2D rb;
+    
     float appearTimer = 0f;
 
 	// Use this for initialization
 	void Start () {
-	
+        transform.position = startPos;
+        rb = GetComponent<Rigidbody2D>();
+        //add a randomized force to the baseball to give a pitch variance
+        rb.AddForce(new Vector3(Random.Range(-maxHSpeed, maxHSpeed), Random.Range(-maxVSpeed, maxVSpeed)));
 	}
 	
 	// Update is called once per frame
